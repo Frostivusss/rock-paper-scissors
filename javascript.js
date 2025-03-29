92ecd0bcf7b3a66219370d6b24b7996936220ad4
Frostivusss <jeromenas09@gmail.com>
2025-03-29 22:22:22 +0800

    complete the task

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-var humanScore = 0;
-var computerScore = 0;
 
 function getComputerChoice() {
     let weapon = (Math.floor(Math.random() * 3) + 1);
@@ -26,33 +24,49 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    //human uses rock. computer uses paper. human lose
-    if (humanChoice === 1 && computerChoice === 2) {
-        computerScore++;
-        console.log("Computer Wins!");
-        // human uses paper. computer uses scissors. human lose
-    } else if (humanChoice === 2 && computerChoice === 3) {
-        computerScore++;
-        console.log("Computer Wins!");
-        // human uses scissors. computer uses rock. human loose.
-    } else if (humanChoice === 3 && computerChoice === 1) {
-        computerScore++;
-        console.log("Computer Wins!");
-        // humae and computer uses the same weapon. tie
-    } else if ((humanChoice === 1 && computerChoice === 1)  ||
-            (humanChoice === 2 && computerChoice === 2) ||
-            (humanChoice === 3 && computerChoice === 3)   
-        ) {
-            console.log("It's a tie!");
 
-    } else {
-        console.log("Human Wins!");
-        humanScore++;
+
+function playGame(playRound) {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for (i=0; i<5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
+
+    function playRound(humanChoice, computerChoice) {
+
+        //human uses rock. computer uses paper. human lose
+        if (humanChoice === 1 && computerChoice === 2) {
+            computerScore++;
+            console.log("Computer Wins!");
+    
+            // human uses paper. computer uses scissors. human lose
+        } else if (humanChoice === 2 && computerChoice === 3) {
+            computerScore++;
+            console.log("Computer Wins!");
+    
+            // human uses scissors. computer uses rock. human loose.
+        } else if (humanChoice === 3 && computerChoice === 1) {
+            computerScore++;
+            console.log("Computer Wins!");
+    
+            // human and computer uses the same weapon. tie
+        } else if ((humanChoice === 1 && computerChoice === 1)  ||
+                (humanChoice === 2 && computerChoice === 2) ||
+                (humanChoice === 3 && computerChoice === 3)   
+            ) {
+                console.log("It's a tie! Restarting the round. Not counted.");
+                i--;
+            //human win
+        } else {
+            console.log("Human Wins!");
+            humanScore++;
+        }
+    }
+
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
