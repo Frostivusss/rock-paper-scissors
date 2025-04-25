3be175bececd30389cd062cad6ad58c7bd0537f7
Frostivusss <jeromenas09@gmail.com>
2025-04-25 17:07:10 +0800

    Refactor the code for the round

--- javascript.js
+++ javascript.js
@@ -1,3 +1,42 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const btn1 = document.querySelector("#rock");
+const btn2 = document.querySelector("#paper");
+const btn3 = document.querySelector("#scissor");
+const list = document.querySelector("ol");
+
+btn1.addEventListener("click", function (e) {
+    const humanChoice = 1;
+    playRound(humanChoice, getComputerChoice());
+});
+
+btn2.addEventListener("click", function (e) {
+    const humanChoice = 2;
+    playRound(humanChoice, getComputerChoice());
+});
+
+btn3.addEventListener("click", function (e) {
+    const humanChoice = 3;
+    playRound(humanChoice, getComputerChoice());
+});
+
+function checkWinner(){
+    //print human wins the game if humanscore is 3 and reset the game
+    if (humanScore >= 3) {
+        alert("Human is the game winner!");
+        list.innerHTML = "";
+        humanScore=0;
+        computerScore=0;
+
+        //print computer wins the game otherwise and reset the game
+    } else if (computerScore > 2) {
+        alert("Computer is the game winner!");
+        list.innerHTML = "";
+        humanScore=0;
+        computerScore=0;
+    }
+}
 
 function getComputerChoice() {
     let weapon = (Math.floor(Math.random() * 3) + 1);
@@ -11,70 +50,39 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let weapon = prompt("Choose your weapon! 1: Rock, 2: Paper, 3: Scissors");
-    weapon = +weapon;
-
-    if (weapon === 1) {
-        return  1;
-    } else if (weapon === 2) {
-         return  2;
-    } else {
-        return  3;
-    }
-}
-
-
-
-function playGame(playRound) {
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    for (i=0; i<5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
     function playRound(humanChoice, computerChoice) {
+
+        const newlist = document.createElement("li");
+        const newText = document.createElement("span");
+        newlist.appendChild(newText);
+        list.appendChild(newlist);
 
         //human uses rock. computer uses paper. human lose
         if (humanChoice === 1 && computerChoice === 2) {
             computerScore++;
-            console.log("Computer Wins the round " + (+i + 1));
+            newText.textContent = ("H=" + humanScore + " vs C=" + computerScore + " || Computer Wins the round ");
     
             // human uses paper. computer uses scissors. human lose
         } else if (humanChoice === 2 && computerChoice === 3) {
             computerScore++;
-            console.log("Computer Wins the round " + (+i + 1));
+            newText.textContent = ("H=" + humanScore + " vs C=" + computerScore + " || Computer Wins the round ");
     
             // human uses scissors. computer uses rock. human loose.
         } else if (humanChoice === 3 && computerChoice === 1) {
             computerScore++;
-            console.log("Computer Wins the round " + (+i + 1));
+            newText.textContent = ("H=" + humanScore + " vs C=" + computerScore + " || Computer Wins the round ");
     
             // human and computer uses the same weapon. tie
         } else if ((humanChoice === 1 && computerChoice === 1)  ||
                 (humanChoice === 2 && computerChoice === 2) ||
                 (humanChoice === 3 && computerChoice === 3)   
             ) {
-                console.log("It's a tie! Restarting the round. Not counted.");
-                i--;
+                newText.textContent = ("H=" + humanScore + " vs C=" + computerScore + " || It's a tie! Restarting the round. Not counted.");
             //human win
         } else {
-            console.log("Human Wins the round " + (+i + 1));
             humanScore++;
+            newText.textContent = ("H=" + humanScore + " vs C=" + computerScore + " || Human Wins the round ");
         }
+
+        checkWinner();
     }
-        //print human wins the game if humanscore is 3
-    if (humanScore >= 3) {
-        console.log("Human is the game winner!");
-
-        //print computer wins the game otherwise
-    } else {
-        console.log("Computer is the game winner!");
-    }
-
-}
-
-playGame();
